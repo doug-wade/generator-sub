bb95510bfad21761999cff3059b161d5f3b0986b
Doug Wade <douglas.b.wade@gmail.com>
2015-11-30 21:16:10 -0800

    Get help from script comments

--- generators/app/templates/lib/example.js
+++ generators/app/templates/lib/example.js
@@ -1,3 +1,9 @@
+/**
+ * An example command.
+ * Usage:
+ *     sub example
+ *     > "You ran the example command!"
+ */
 module.exports = function() {
   console.log("You ran the example command!");
 }
