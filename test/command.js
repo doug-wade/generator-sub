191415d7fe2086ea6d53977abd19368019a18d21
Doug Wade <douglas.b.wade@gmail.com>
2018-12-17 20:07:33 -0800

    Resolve linting problems

--- test/command.js
+++ test/command.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import helpers from 'yeoman-test';
 import test from 'ava';
 
-test(`generator-sub:command creates file`, async t => {
+test('generator-sub:command creates file', async t => {
 	let testDir;
 	await helpers.run(path.join(__dirname, '../generators/app'))
 		.inTmpDir(dir => {
@@ -20,7 +20,7 @@ function exists(filename, dir) {
 	try {
 		fs.accessSync(filename, fs.F_OK);
 		return true;
-	} catch (err) {
+	} catch (error) {
 		return false;
 	}
 }
